0f4d034d66176fdbde4b7f6537b4a6990eff5a33
Leandro Poblet <leandrodrhouse@gmail.com>
2014-01-24 19:35:51 -0300

    Code refactory

--- edison.py
+++ edison.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 import os
+import re
 import sys
 import urllib
 import time
@@ -14,310 +15,92 @@ class Edison(object):
 	"""docstring for Edison"""
 
 	def start(self, site, key):
-		self.req = requests.get('http://api.tumblr.com/' \
-		'v2/blog/%s/info?api_key=%s' % (site, key))
-		if (self.req.status_code == 200):
-			self.req = self.req.json()
-			self.req = simplejson.loads(simplejson.dumps(self.req))
-			return self.req
+		self.meta = requests.get('http://api.tumblr.com/' \
+		'v2/blog/%s.tumblr.com/info?api_key=%s' % (site, key))
+		if (self.meta.status_code == 200):
+			self.meta = self.meta.json()
+			print('Blog title: ', self.meta['response']['blog']['title'],\
+				'\nBlog name: ', self.meta['response']['blog']['name'], \
+				'\nBlog URL: ', self.meta['response']['blog']['url'], \
+				'\nStarting Conversion....\n\n')
 		else:
-			print('Error of status %s: %s'\
-					% (self.req['meta']['status'], self.req['meta']['msg']))
+			print('Error!')
 			sys.exit(1)
 
-	def fetch_all_posts(self, site, key,
-						limit=0):
-		if (limit == 0):
-			self.posts = requests.get('http://api.tumblr.com/' \
-				'v2/blog/%s/posts/?api_key=%s' % (site, key))
-		else:
-				self.posts = requests.get('http://api.tumblr.com/' \
-					'v2/blog/%s/posts/?api_key=%s&limit=%d' % (site, key, limit))
-		self.posts = simplejson.loads(simplejson.dumps(self.posts.json()))
-		self.posts = self.posts['response']['posts']
-		return self.posts
 
-	def make_Nikola_imgPost(self, post_index, total, folder='tumblr/'):
-		self.index = 0
-		while (self.index != total):
-			self.post = post_index[self.index]
-			self.image = self.post['photos'][0]['original_size']['url']
+	def download_text_posts(self, post):
+		print('Downloading text post #',post['id'],'...')
+		try:
+			self.new_post = open('%s.txt' % post['slug'], 'wb')
+		except IOError:
+			return "Impossible to create new post."
+			sys.exit(1)
+
+		self.Npost = """.. link: %s\r
+.. description: %s\r
+.. tags: %s\r
+.. date: %s\r
+.. title: %s\r
+.. slug: %s\r
+\r
+%s""" % (str(post['slug']), post['title'],
+				['' if len(post['tags']) <= 0 else post['tags']][0][0],
+				post['date'], post['id'],
+				post['slug'], re.sub('<[^<]+?>', '', post['body']).decode())
+
+		self.new_post.write(self.Npost)
+		self.new_post.close()
+		print('Post #',post['id'],' completed!')
+
+
+	def download_image_posts(self, post):
+		print('Downloading image post #',post['id'],'...')
+		if (len(post['photos']) == 1):
+			self.image = post['photos'][0]['original_size']['url']
 			self.image_name, self.image_extension = os.path\
-				.splitext(self.image)
-			# The original filename does not exist, the Tumblr hash is
-			# quite shitty and I prefer to use the timestamp
-			self.image_name = self.post['reblog_key']
-			self.output = folder + self.image_name\
-								 + self.image_extension
+							.splitext(self.image)
+
+			# This one's cleaner than the original filename
+			self.image_name = re.sub('tumblr_.*_[0-9]+.*', '', self.image_name)
+			self.image_name = self.image_name.split('/')[-2]
+			self.output = self.image_name + self.image_extension
+
 			urllib.urlretrieve(self.image, '%s' % (self.output))
-			try:
-				self.new_post = open('%s.txt' % self.post['id'], 'wb')
-			except IOError:
-				return "Impossible to create new post."
-				sys.exit(1)
 
-			self.Npost = """.. link: %s
-.. description: %s 
-.. tags: %s
-.. date: %s
-.. title: %s
-.. slug: %s
+		if (len(post['photos']) > 1):
+			for photoset in post['photos']:
+				self.image = photoset['original_size']['url']
+				self.image_name, self.image_extension = os.path\
+								.splitext(self.image)
 
-.. Image:: %s
-%s""" % (post_index['title'], post_index['title'],
-				post_index['tags'], post_index['date'],
-				post_index['id'], post_index['slug'],
-				post_index['body'])
-			self.post_path = folder + 'stories/'
-			if not os.path.exists(self.post_path):
-				os.makedirs(self.post_path)
-				os.chdir(self.post_path)
-			else:
-				os.chdir(self.post_path)			
-			self.new_post.write(self.Npost)
-			self.new_post.close()
-			# With this timer the code should be able
-			# to avoid Tumblr's shutdown on its own API
-			time.sleep(3)
-			self.index = self.index + 1
+				# This one's cleaner than the original filename
+				self.image_name = re.sub('tumblr_.*_[0-9]+.*', '', self.image_name)
+				self.image_name = self.image_name.split('/')[-2]
+				self.output = self.image_name + self.image_extension
 
-	def fetch_image_posts(self, site, key, limit=0,
-						  folder='tumblr/'):
+				urllib.urlretrieve(self.image, '%s' % (self.output))				
+
+		print('Post #',post['id'],' completed!')
+
+
+	def download_all_posts(self, site, key, folder="tumblr/"):
 		if not os.path.exists(folder):
 			os.makedirs(folder)
-
-		self.img_posts = requests.get('http://api.tumblr.com/'\
-			'v2/blog/%s/posts?api_key=%s&type=photo' % (site, key))
-
-		if (limit != 0):
-			self.img_posts = requests.get('http://api.tumblr.com/'\
-				'v2/blog/%s/posts?api_key=%s&type=photo&limit=' \
-				% (site, key, self.img_posts['response']['total_posts']))
-			self.img_posts = simplejson.loads(simplejson\
-				.dumps(self.img_posts.json()))
-			self.make_Nikola_imgPost(self.img_posts['response']['posts'],
-									 20, folder) # Tumblr's default load
-		self.make_Nikola_imgPost(self.img_posts['response']['posts'],
-								 limit, folder)
-
-	def make_Nikola_textPost(self, post_index, total, folder='tumblr/'):
-		if (total == 0):
-			return "Nothing to import."
-		elif (total == 1):
-			try:
-				self.new_post = open('%s.txt' % post_index['title'], 'wb')
-			except IOError:
-				return "Impossible to create new post."
-				sys.exit(1)
-
-			self.Npost = """.. link: %s
-.. description: %s 
-.. tags: %s
-.. date: %s
-.. title: %s
-.. slug: %s
-
-%s""" % (post_index['title'], post_index['title'],
-			['' if len(post_index['tags']) <= 0 else post_index['tags']][0],
-			post_index['date'], post_index['id'],
-			post_index['slug'], post_index['body'])
-
-			self.post_path = folder + 'stories/'
-			if not os.path.exists(self.post_path):
-				os.makedirs(self.post_path)
-				os.chdir(self.post_path)
-			else:
-				os.chdir(self.post_path)			
-			self.new_post.write(self.Npost)
-			self.new_post.close()
+			os.chdir(folder)
 		else:
+			os.chdir(folder)
+		self.start(site, key)
+		self.posts = requests.get('http://api.tumblr.com' \
+			'/v2/blog/%s.tumblr.com/posts/?api_key=%s' % (site, key))
+		if (self.posts.status_code == 200):
+			self.posts = self.posts.json()
+			self.total_posts = self.posts['response']['total_posts']
+			self.posts_content = self.posts['response']['posts']
 			self.index = 0
-			while (self.index != total):
-				try:
-					self.new_post = open('%s.txt' % post_index['title'], 'wb')
-				except IOError:
-					return "Impossible to create new post."
-					sys.exit(1)
-
-				if(post_index['title'] != ''):
-					self.Npost = """.. link: %s
-.. description: %s 
-.. tags: %s
-.. date: %s
-.. title: %s
-.. slug: %s
-
-%s""" % (post_index['title'], post_index['title'],
-					post_index['tags'], post_index['date'],
-					post_index['id'], post_index['slug'],
-					post_index['body'])
-
-				self.post_path = folder + 'stories/'
-				if not os.path.exists(self.post_path):
-					os.makedirs(self.post_path)
-					os.chdir(self.post_path)
-				else:
-					os.chdir(self.post_path)		
-				self.new_post.write(self.Npost)
-				self.new_post.close()
-			else:
-				self.Npost = """.. link: %s
-.. description: %s 
-.. tags: %s
-.. date: %s
-.. title: %s
-.. slug: %s
-
-%s""" % (post_index['id'], post_index['id'],
-			['' if len(post_index['tags']) <= 0 else post_index['tags']][0],
-			post_index['date'], post_index['id'],
-			post_index['slug'], post_index['body'])
-
-			self.post_path = folder + 'stories/'
-			if not os.path.exists(self.post_path):
-				os.makedirs(self.post_path)
-				os.chdir(self.post_path)
-			else:
-				os.chdir(self.post_path)	
-			self.new_post.write(self.Npost)
-			self.new_post.close()
-			# With this timer the code should be able
-			# to avoid Tumblr's shutdown on its own API
-			time.sleep(3)
-			self.index = self.index + 1
-
-	def make_Nikola_quotePost(self, post_index, total, folder='tumblr/'):
-		if (total == 0):
-			return "Nothing to import."
-		elif (total == 1):
-			try:
-				self.new_post = open('%s.txt' % post_index['slug'], 'wb')
-			except IOError:
-				return "Impossible to create new post."
-				sys.exit(1)
-
-			self.quote = "%s - %s" % (post_index['text'], post_index['source'])
-			print(type(self.quote))
-			self.Npost = """.. link: %s
-.. description: %s 
-.. tags: %s
-.. date: %s
-.. title: %s
-.. slug: %s
-
-%s""" % (post_index['slug'], post_index['text'],
-			['' if len(post_index['tags']) <= 0 else post_index['tags']][0],
-			post_index['date'], post_index['text'],
-			post_index['slug'], self.quote)
-
-			self.post_path = folder + 'stories/'
-			if not os.path.exists(self.post_path):
-				os.makedirs(self.post_path)
-				os.chdir(self.post_path)
-			else:
-				os.chdir(self.post_path)
-			# self.new_post.write(self.Npost)
-			# self.new_post.close()
-		else:
-			self.index = 0
-			while (self.index != total):
-				try:
-					self.new_post = open('%s.txt' % post_index['title'], 'wb')
-				except IOError:
-					return "Impossible to create new post."
-					sys.exit(1)
-
-				if(post_index['title'] != ''):
-					self.quote = "%s - %s" % (post_index['text'],
-											  post_index['source'])
-				self.Npost = """.. link: %s
-.. description: %s 
-.. tags: %s
-.. date: %s
-.. title: %s
-.. slug: %s
-
-%s""" % (post_index['slug'], post_index['text'],
-					['' if len(post_index['tags']) <= 0 \
-						else post_index['tags']][0],
-					post_index['date'], post_index['text'],
-					post_index['slug'], self.quote)
-
-				self.post_path = folder + 'stories/'
-				if not os.path.exists(self.post_path):
-					os.makedirs(self.post_path)
-					os.chdir(self.post_path)
-				else:
-					os.chdir(self.post_path)		
-				self.new_post.write(self.Npost)
-				self.new_post.close()
-			else:
-				self.quote = "%s - %s" % (post_index['text'],
-										  post_index['source'])
-				self.Npost = """.. link: %s
-.. description: %s 
-.. tags: %s
-.. date: %s
-.. title: %s
-.. slug: %s
-
-%s""" % (post_index['slug'], post_index['text'],
-				['' if len(post_index['tags']) <= 0\
-					else post_index['tags']][0],
-				post_index['date'], post_index['text'],
-				post_index['slug'], self.quote)
-
-			self.post_path = folder + 'stories/'
-			if not os.path.exists(self.post_path):
-				os.makedirs(self.post_path)
-				os.chdir(self.post_path)
-			else:
-				os.chdir(self.post_path)
-			self.new_post.write(self.Npost)
-			self.new_post.close()
-				# With this timer the code should be able
-				# to avoid Tumblr's shutdown on its own API
-			time.sleep(3)
-			self.index = self.index + 1		
-
-	def fetch_text_posts(self, site, key, get_type, limit=0,
-						 folder='tumblr/'):
-		"""
-			Note that this function works on text, quote, link and chat
-			all at one. It won't download images inside the posts but it
-			will link them directly, which might be changed in the future
-			after Nikola supports a basic image uploading system, or the
-			user might as well do it manually.
-		"""
-		if (get_type == 'text'):
-			self.text_posts = requests.get('http://api.tumblr.com/v2/'\
-				'blog/%s/posts?api_key=%s&type=%s'\
-				% (site, key, get_type))
-			self.text_posts = self.text_posts.json()
-			self.text_posts = simplejson.loads(simplejson\
-							  .dumps(self.text_posts))
-			self.total_posts = self.text_posts['response']['total_posts']
-			if (limit != 0):
-				self.text_posts = requests.get('http://api.tumblr.com/'\
-					'v2/blog/%s/posts?api_key=%s&type=%s&limit'\
-					% (site, key, get_type,
-					   self.text_posts['response']['total_posts']))
-				self.text_posts = simplejson.loads(simplejson\
-							  		.dumps(self.text_posts))
-			self.all_text_posts = self.text_posts['response']['posts'][0]
-			self.make_Nikola_textPost(self.all_text_posts, self.total_posts)
-		elif (get_type == 'quote'):
-			self.quote_posts = requests.get('http://api.tumblr.com/v2/'\
-				'blog/%s/posts?api_key=%s&type=%s'\
-				% (site, key, get_type))
-			self.quote_posts = self.quote_posts.json()
-			self.quote_posts = simplejson.loads(simplejson\
-								.dumps(self.quote_posts))
-			self.total_posts = self.quote_posts['response']['total_posts']
-			if limit != 0:
-				self.quote_posts = requests.get('http://api.tumblr.com/'\
-					'v2/blog/%s/posts?api_key=%s&type=%s&limit'\
-					% (site, key, get_type,
-					   self.text_posts['response']['total_posts']))
-			self.all_quote_posts = self.quote_posts['response']['posts'][0]
-			self.make_Nikola_quotePost(self.all_quote_posts, self.total_posts)
+			while (self.index != self.total_posts):
+				self.current_post = self.posts_content[self.index]
+				if (self.current_post['type'] == 'photo'):
+					self.download_image_posts(self.current_post)
+				if (self.current_post['type'] == 'text'):
+					self.download_text_posts(self.current_post)
+				self.index = self.index + 1
