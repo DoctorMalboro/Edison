d1f59ca8c501e27c1f6286e7ffac27bd8856cba2
Leandro Poblet <leandrodrhouse@gmail.com>
2014-01-27 19:55:47 -0300

    First Edison draft

--- edison.py
+++ edison.py
@@ -33,7 +33,7 @@ class Edison(object):
 	def download_text_posts(self, post):
 		print('Downloading text post #',post['id'],'...')
 		try:
-			self.new_post = open('%s.txt' % post['slug'], 'wb')
+			self.new_post = open('%s.rst' % post['slug'], 'wb')
 		except IOError:
 			return "Impossible to create new post."
 			sys.exit(1)
@@ -44,6 +44,7 @@ class Edison(object):
 .. date: %s\r
 .. title: %s\r
 .. slug: %s\r
+.. type: text\r
 \r
 %s""" % (str(post['slug']), post['title'],
 				['' if len(post['tags']) <= 0 else post['tags']][0][0],
@@ -87,6 +88,10 @@ class Edison(object):
 
 
 	def audio_from_tumblr(self, post):
+		"""
+		THIS FUNCIONALITY IS COMPLETELY EXPERIMENTAL!!!
+		USE ONLY FOR TESTING PURPOSES!!!
+		"""
 		self.audio_name, self.audio_extension = os.path\
 							.splitext(post['audio_url'])
 		print(self.audio_name)
@@ -96,8 +101,8 @@ class Edison(object):
 		urllib.urlretrieve(post['audio_url'], self.output_audio)
 
 		client = soundcloud.Client(
-			client_id='58a7a68a31f68bdf4536c72f4542257d',
-			client_secret='a566853752f6f9833508c3a6097691b7',
+			client_id='',
+			client_secret='',
 		)
 		webbrowser.open(urlclient.authorize_url())
 
@@ -107,21 +112,110 @@ class Edison(object):
 		})
 
 	def audio_from_soundcloud(self, post):
-		pass
+		print('Downloading audio post #',post['id'],'...')
+		current_url = re.findall(r'[0-9]+', post['audio_url'])[0]
+		slug_name = re.sub(r'\s+', '-', post['track_name'])
+
+		try:
+			self.new_post = open('%s.rst' % slug_name, 'wb')
+		except IOError:
+			return "Impossible to create new post."
+			sys.exit(1)
+
+		if (post['caption'] != ''):
+			self.Npost = """.. link: %s\r
+.. description: %s\r
+.. tags: \r
+.. date: %s\r
+.. title: %s\r
+.. slug: %s\r
+.. type: text\r
+\r
+.. soundcloud:: %s\r
+
+%s""" % (slug_name, slug_name,
+				post['date'], post['id'],
+				post['track_name'], current_url,
+				post['caption'])
+
+		else:
+			self.Npost = """.. link: %s\r
+.. description: %s\r
+.. tags: \r
+.. date: %s\r
+.. title: %s\r
+.. slug: %s\r
+.. type: text\r
+\r
+.. soundcloud:: %s""" % (slug_name, slug_name,
+				post['date'], post['id'],
+				post['track_name'], current_url)			
+
+		self.new_post.write(self.Npost)
+		self.new_post.close()
+
+		print('Post #',post['id'],' completed!')
 
 	def download_audio_posts(self, post):
-		if (post['audio_type'] == 'tumblr'):
-			self.audio_from_tumblr(post)
 		if (post['audio_type'] == 'soundcloud'):
 			self.audio_from_soundcloud(post)
 
+
+	def download_video_posts(self, post):
+		print('Downloading video post #',post['id'],'...')
+		video_id = re.findall(
+			"#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]" \
+			+ "+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#",
+			post['permalink_url'])[0]
+
+
+		try:
+			self.new_post = open('%s.rst' % post['id'], 'wb')
+		except IOError:
+			return "Impossible to create new post."
+			sys.exit(1)
+
+		if (post['caption'] != ''):
+			self.Npost = """.. link: %s\r
+.. description: %s\r
+.. tags: \r
+.. date: %s\r
+.. title: %s\r
+.. slug: %s\r
+.. type: text\r
+\r
+.. youtube:: %s\r
+
+%s""" % (post['id'], post['id'],
+				post['date'], post['permalink_url'],
+				post['id'], video_id,
+				post['caption'])
+
+		else:
+			self.Npost = """.. link: %s\r
+.. description: %s\r
+.. tags: \r
+.. date: %s\r
+.. title: %s\r
+.. slug: %s\r
+.. type: text\r
+\r
+.. youtube:: %s""" % (post['id'], post['permalink_url'],
+				post['date'], post['id'],
+				post['id'], video_id)			
+
+		self.new_post.write(self.Npost)
+		self.new_post.close()
+
+		print('Post #',post['id'],' completed!')
+
 	def download_all_posts(self, site, key, folder="tumblr/"):
+		self.start(site, key)
 		if not os.path.exists(folder):
 			os.makedirs(folder)
 			os.chdir(folder)
 		else:
 			os.chdir(folder)
-		self.start(site, key)
 		self.posts = requests.get('http://api.tumblr.com' \
 			'/v2/blog/%s.tumblr.com/posts/?api_key=%s' % (site, key))
 		if (self.posts.status_code == 200):
@@ -131,6 +225,8 @@ class Edison(object):
 			self.index = 0
 			while (self.index != self.total_posts):
 				self.current_post = self.posts_content[self.index]
+				if (self.current_post['type'] == 'video'):
+					self.download_video_posts(self.current_post)
 				if (self.current_post['type'] == 'audio'):
 					self.download_audio_posts(self.current_post)
 				if (self.current_post['type'] == 'photo'):
---
+++ main.py
@@ -0,0 +1,21 @@
+from __future__ import print_function, unicode_literals
+from edison import Edison
+
+
+def main():
+
+	convert = Edison()
+
+	print('Welcome to Edison! The Tumblr to Nikola converter\n')
+	print('Once its done, remember to move the files to their respective folder')
+	print('\n\n')
+
+	blog_url = raw_input('Insert blog URL (without the .tumblr.com): ')
+	print('\n\n')
+
+	convert.download_all_posts(blog_url, 'RhuQFqBllfWp7P77FEEcKFzoWkqt2AWyHAt4znyriCxlSTBfft')
+
+	print("\n\nDone! Enjoy your Nikola website!")
+
+if __name__ == "__main__":
+	main()
