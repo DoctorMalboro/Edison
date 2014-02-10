ab1dc625aa61a1d6ecfda0ee2c9a8d641828cc09
Leandro Poblet <leandrodrhouse@gmail.com>
2014-02-10 16:10:12 -0300

    Added config file support.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from __future__ import print_function, unicode_literals
 from edison import Edison
+import json
 
+# Use for personal use, do not push with this on True
+DEBUG = True
 
 def main():
+
+	try:
+		if DEBUG == True:
+			config_file = open('config_dev.json')
+		else:
+			config_file = open('config.json')
+	except IOError:
+		print("Couldn't open the config file.")
+
+	config = json.load(config_file)
 
 	convert = Edison()
 
@@ -13,7 +26,7 @@ def main():
 	blog_url = raw_input('Insert blog URL (without the .tumblr.com): ')
 	print('\n\n')
 
-	convert.download_all_posts(blog_url, '')
+	convert.download_all_posts(blog_url, config['settings']['API_KEY'])
 
 	print("\n\nDone! Enjoy your Nikola website!")
 
